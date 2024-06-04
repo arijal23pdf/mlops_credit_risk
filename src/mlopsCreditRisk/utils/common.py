01852a3b1a6724218bef35bcf589fc81823e2129
ASHIM RIJAL <rijalashim@gmail.com>
2024-06-04 16:43:23 +0200

    fix package name to resolve import error in common.py

--- src/mlopsCreditRisk/utils/common.py
+++ src/mlopsCreditRisk/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from mlopsCreditRisk import logger
 import json
 import joblib
 from ensure import ensure_annotations
