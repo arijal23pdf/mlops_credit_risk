73c910ada5182ebb24a6aec3abede28eb4446f68
ASHIM RIJAL <rijalashim@gmail.com>
2024-06-05 17:07:55 +0200

    add data validation pipeline to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlopsCreditRisk import logger
 from mlopsCreditRisk.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlopsCreditRisk.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -10,5 +11,16 @@ try:
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
